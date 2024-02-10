c4ce22305803decb8f2e3ed18fdd8d58d91c1ac0
doora <dorasyu1202@gmail.com>
2024-02-10 18:40:44 +0900

    receive apikey

--- app/controllers/MailcopilotController.rb
+++ app/controllers/MailcopilotController.rb
@@ -1,19 +1,8 @@
 require "open_ai.rb"
 
 class MailcopilotController < ActionController::API
-    def register
-        @apikey = Apikey.new(apikey_params)
-        @apikey.save
-    end
-
     def index
         mail = OpenAi.generate_mail(params[:apikey],params[:subject])
         render json: mail
-    end
-
-    private
-    
-    def apikey_params
-        params.permit(:apikey)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
-  post 'mail_copilot/register/:apikey', :to =>'mailcopilot#register'
-  get 'mail_copilot/:subject', :to =>'mailcopilot#index'
+  get 'mail_copilot/:apikey/:subject', :to =>'mailcopilot#index'
 
 end
