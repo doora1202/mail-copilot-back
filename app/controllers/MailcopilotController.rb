699e9eb1b669ce3136826b8686ca6c5752dcf4a3
doora <dorasyu1202@gmail.com>
2024-02-10 12:39:01 +0900

    local api

--- app/controllers/MailcopilotController.rb
+++ app/controllers/MailcopilotController.rb
@@ -2,7 +2,7 @@ require "open_ai.rb"
 
 class MailcopilotController < ActionController::API
     def index
-        mail = OpenAi.generate_mail(params[:apikey],params[:subject])
+        mail = OpenAi.generate_mail(params[:subject])
         render json: mail
     end
 end
--- app/models/open_ai.rb
+++ app/models/open_ai.rb
@@ -1,10 +1,10 @@
 class OpenAi < ApplicationRecord
-    def self.generate_mail(apikey,subject)
-        client = OpenAI::Client.new(access_token: apikey)
+    def self.generate_mail(subject)
+        client = OpenAI::Client.new
         response = client.chat(
         parameters: {
             model: 'gpt-3.5-turbo',
-            messages: [{ role: 'user', content: "メールの件名は#{subject}です。本文を書いてください なおできるだけ短い文章で書いてください" }],
+            messages: [{ role: 'user', content: "メールの件名は#{subject}です。本文を書いてください" }],
         }
     )
     response.dig('choices', 0, 'message', 'content')
