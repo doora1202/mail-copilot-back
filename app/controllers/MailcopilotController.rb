9e77dbb4fe7bf1701056232109f8c1c6a2087d99
doora <dorasyu1202@gmail.com>
2024-03-22 00:00:24 +0900

    本文から件名を生成可能に変更

--- app/controllers/MailcopilotController.rb
+++ app/controllers/MailcopilotController.rb
@@ -1,8 +1,15 @@
 require "open_ai.rb"
 
 class MailcopilotController < ActionController::API
-    def index
-        mail = OpenAi.generate_mail(params[:apikey],params[:subject])
-        render json: mail
+    def generate
+        if params[:id] == "1" then
+            mail = OpenAi.generate_text(params[:apikey],params[:str])
+            render json: mail
+        elsif params[:id] == "2" then
+            mail = OpenAi.generate_subject(params[:apikey],params[:str])
+            render json: mail
+        else
+            render json: { error: "Invalid id" }, status: :unprocessable_entity
+        end
     end
 end
--- app/models/open_ai.rb
+++ app/models/open_ai.rb
@@ -1,10 +1,20 @@
 class OpenAi < ApplicationRecord
-    def self.generate_mail(apikey,subject)
+    def self.generate_text(apikey,subject)
         client = OpenAI::Client.new(access_token: apikey)
         response = client.chat(
         parameters: {
             model: 'gpt-3.5-turbo',
             messages: [{ role: 'user', content: "メールの件名は#{subject}です。本文のみを敬語を使って書いてください。" }],
+        }
+    )
+    response.dig('choices', 0, 'message', 'content')
+    end
+    def self.generate_subject(apikey,text)
+        client = OpenAI::Client.new(access_token: apikey)
+        response = client.chat(
+        parameters: {
+            model: 'gpt-3.5-turbo',
+            messages: [{ role: 'user', content: "メールの本文は#{text}です。件名を書いてください。" }],
         }
     )
     response.dig('choices', 0, 'message', 'content')
